3b8c55e54e4d76b82732bc0a9b8749a325099d73
dmeats <djmmm@hotmail.com>
2020-11-19 15:17:40 -0500

    Updates

---
+++ precache-manifest.2c08a4b855c7e400f1eb360aaeefa0e9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78561f5e84ec46bc1b73711a0f7f812",
+    "url": "/drum-machine-mk3/index.html"
+  },
+  {
+    "revision": "efffb177d3e0f3ba5ff4",
+    "url": "/drum-machine-mk3/static/css/main.9294be42.chunk.css"
+  },
+  {
+    "revision": "7e3e181bc016c610bff8",
+    "url": "/drum-machine-mk3/static/js/2.ce142ee9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/drum-machine-mk3/static/js/2.ce142ee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efffb177d3e0f3ba5ff4",
+    "url": "/drum-machine-mk3/static/js/main.20611e74.chunk.js"
+  },
+  {
+    "revision": "c46205b6f1619ca096ae",
+    "url": "/drum-machine-mk3/static/js/runtime-main.e5d7c257.js"
+  },
+  {
+    "revision": "bfd9c003c3613c486a6c30b2a54bafff",
+    "url": "/drum-machine-mk3/static/media/cabase.bfd9c003.mp3"
+  },
+  {
+    "revision": "7617f9c5474c80672c3b6e22968d122c",
+    "url": "/drum-machine-mk3/static/media/conga.7617f9c5.mp3"
+  },
+  {
+    "revision": "ca647233465c5f8bc5bad835451e65d6",
+    "url": "/drum-machine-mk3/static/media/kick.ca647233.mp3"
+  },
+  {
+    "revision": "6e4f8c143b7717dfb67dcc059b4aff63",
+    "url": "/drum-machine-mk3/static/media/kick2.6e4f8c14.mp3"
+  },
+  {
+    "revision": "7dba5ee36a08d65696e2fa1a8c429555",
+    "url": "/drum-machine-mk3/static/media/lotom.7dba5ee3.mp3"
+  },
+  {
+    "revision": "cb0438ab4947ab4d1f9c61c66c18e583",
+    "url": "/drum-machine-mk3/static/media/snare.cb0438ab.mp3"
+  },
+  {
+    "revision": "fca83d165d8e7ba9f8216adf68d787ed",
+    "url": "/drum-machine-mk3/static/media/snare2.fca83d16.mp3"
+  },
+  {
+    "revision": "1f3b9b77dd5036072aef4591cf9c9d59",
+    "url": "/drum-machine-mk3/static/media/timbale.1f3b9b77.mp3"
+  },
+  {
+    "revision": "feaae22f0de61bf911c3f3faaa481cb2",
+    "url": "/drum-machine-mk3/static/media/tomlo.feaae22f.mp3"
+  }
+]);
